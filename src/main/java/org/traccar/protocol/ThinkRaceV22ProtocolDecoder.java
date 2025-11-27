c609d46ee693b9d95e2c8a7b1d26c543b7e593e8
Mamadou Lamine DIARRA <msl.diarra@gmail.com>
2025-11-27 20:53:46 +0000

    Protocol Thinktrace : Trying to make wifi part of the request of gelocation

--- src/main/java/org/traccar/protocol/ThinkRaceV22ProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/ThinkRaceV22ProtocolDecoder.java
@@ -56,26 +56,19 @@ public class ThinkRaceV22ProtocolDecoder extends BaseProtocolDecoder {
             .number(",(dd)").optional()          // alarm
             .groupBegin()
             .text(",")
-            .expression("(")
-            .groupBegin()
-            .expression("[^|]+")                          // name (AP1, AP2, etc.)
-            .expression("\\|([0-9a-fA-F:]+)\\|")         // MAC address (hex avec :)
-            .expression("(-?\\d+)")                       // signal strength (peut être négatif)
-            .expression("&?")                             // optional separator
-            .groupEnd("+")
-            .expression(")")
+            .expression("([^#]+)")               // WiFi data (tout jusqu'au #)
             .groupEnd("?")
             /* Original wifi code */
             /* .groupBegin()
             .text(",")
             .expression("(")
             .groupBegin()
-            .expression("[^\\|]+")               // name
+            .expression("[^|]+")               // name
             .number("|xx-xx-xx-xx-xx-xx")        // mac
             .number("|d+&?")                     // signal
-            .groupEnd("+")*/
+            .groupEnd("+")
             .expression(")")
-            .groupEnd("?")
+            .groupEnd("?")*/
             .any()
             .compile();
 
